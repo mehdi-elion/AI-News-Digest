5a8cc10c6ee020735fa39a109e1ab03a7f19952d
Mehdi Elion <mehdi.elion@gmail.com>
2023-11-05 20:40:03 +0100

    [LINT] applied pre-commit and minor linting

--- src/code/load_arxiv_corpus.py
+++ src/code/load_arxiv_corpus.py
@@ -1,8 +1,9 @@
-from rich import print
-import arxiv
-import json
 import datetime
+import json
 from pathlib import Path
+from typing import Any, Dict
+
+import arxiv
 from loguru import logger
 from tqdm import tqdm
 
@@ -11,18 +12,17 @@ QUERIES = [
     "ti:deep AND ti:learning",
     "ti:transformer",
     "abs:explainability AND abs:neural",
-    "abs:interpretability AND abs:neural"
+    "abs:interpretability AND abs:neural",
 ]
 MAX_RESULTS = 4
 OUTPUT_FOLDER = "data/03_primary/"
 
 
-if __name__=="__main__":
-    
+if __name__ == "__main__":
     # get today's date
     now = str(datetime.datetime.now()).split(".")[0]
     now = now.replace(" ", "_").replace(":", "-")
-    
+
     # create output file name
     output_filename = Path(OUTPUT_FOLDER) / f"arxiv_dict_{now}.json"
     logger.info(f"Will run arxiv search and output results in {output_filename}...")
@@ -31,15 +31,14 @@ if __name__=="__main__":
     res_dict = {}
 
     # iterate over queries
-    logger.info(f"Proceeding to individual queries...")
+    logger.info("Proceeding to individual queries...")
     for i, query in enumerate(tqdm(QUERIES)):
-        
         # run query via arxiv API
         logger.info(f"Running query '{query}' ({i+1}/{len(QUERIES)})...")
         search = arxiv.Search(
             query=query,
             max_results=MAX_RESULTS,
-            sort_by=arxiv.SortCriterion.SubmittedDate
+            sort_by=arxiv.SortCriterion.SubmittedDate,
         )
 
         # parse results
@@ -50,17 +49,11 @@ if __name__=="__main__":
                 "date": str(result.published),
                 "category": result.primary_category,
             }
-        
+
     # store results
-    over_dict = {}
+    over_dict: Dict[str, Any] = {}
     over_dict["queries"] = QUERIES
     over_dict["results"] = res_dict
     with open(output_filename, "w") as f:
         json.dump(over_dict, f)
     logger.success(f"Successfully saved results to '{output_filename}' !")
-
-            
-
-
-
-
